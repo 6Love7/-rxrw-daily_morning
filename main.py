94bf8909e0e0ab00901e16f217d17f96f3ce48fd
6Love7 <115070018+6Love7@users.noreply.github.com>
2023-08-07 15:51:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -162,7 +162,7 @@ data = {
     #     "color": get_random_color()
     # },
     "temperature": {
-        "value": math.floor(weather['temperature']),
+        "value": weather['temperature'],
         "color": get_random_color()
     },
     # "highest": {
